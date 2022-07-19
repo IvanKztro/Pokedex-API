7deece79862f9d4e3b2ac37d4164084b97d7a104
IvanCastroR <ivan@unozerocode.com>
2022-07-19 11:15:27 -0500

    clean code

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,11 +1,8 @@
-import Sidebar from "./sidebar";
 import Navbar from "./nav";
 
 export default function Layout({ children }) {
   return (
     <>
-      {/* <div className="content">
-        <Sidebar className="sidebar" /> */}
       <Navbar className="navbar" />
       <main id="main" className="main">
         {children}
--- components/nav.jsx
+++ components/nav.jsx
@@ -4,7 +4,6 @@ export default function Navbar() {
       <nav className="bg-red-600 px-3 py-3 text-white fixed w-full z-10 top-0">
         <h2 className="font-bold text-[2rem]">Pokedex</h2>
       </nav>
-      <slot name="sidebar" />
     </>
   );
 }
